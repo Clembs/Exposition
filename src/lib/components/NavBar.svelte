51dd7b4c93048de630302ce1ff18946a678cc53e
Clément Voisin <clembs@clembs.com>
2024-03-14 12:04:32 +0100

    feat: navbar styling

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,9 +1,44 @@
 <nav>
 	<a href="/">Equipe</a>
-	<div>
-		<a href="/programme">Programme</a>
-		<a href="/billeterie">Billeterie</a>
-		<a href="/infos">Infos</a>
-		<a href="/contact">Contact</a>
-	</div>
+	<ul>
+		<li>
+			<a href="#à-propos">À propos</a>
+		</li>
+		<li>
+			<a href="#galerie">Galerie</a>
+		</li>
+		<li>
+			<a href="/contact">Contact</a>
+		</li>
+		<li>
+			<a id="billets" href="#billeterie">Billets</a>
+		</li>
+	</ul>
 </nav>
+
+<style lang="scss">
+	nav {
+		display: flex;
+		justify-content: space-between;
+		padding: 0.5rem 1rem;
+		position: sticky;
+		inset: 0;
+		align-items: center;
+
+		ul {
+			display: flex;
+			gap: 0.25rem;
+			align-items: center;
+
+			a {
+				display: block;
+				padding: 0.875rem 1.5rem;
+				border-radius: 99rem;
+				&#billets {
+					background-color: var(--color-shell-black);
+					color: white;
+				}
+			}
+		}
+	}
+</style>
