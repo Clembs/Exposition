6721d27a6b297eaa9beedff3c89847f2670beae2
Gérald Leban <gerald.leban0@gmail.com>
2024-03-15 21:50:13 +0100

    feat: add find us section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Hero from './Hero.svelte';
+	import FindUs from './FindUs.svelte';
 </script>
 
 <main>
 	<Hero />
+	<FindUs />
 </main>
---
+++ src/routes/FindUs.svelte
@@ -0,0 +1,60 @@
+<section id="find-us">
+	<img src="https://w.wallhaven.cc/full/l8/wallhaven-l8vp7y.jpg" alt="" />
+	<div class="aero-card">
+		<div>
+			<h1>Où nous trouver ?</h1>
+			<p>Au 1er étage de l'IUT Informatique de Toulouse, 133b Av de Rangueil</p>
+
+			<iframe
+				title="map to crdoc"
+				src="https://www.openstreetmap.org/export/embed.html?bbox=1.4628499746322634%2C43.5695478655846%2C1.4663904905319216%2C43.57131630006121&amp;layer=mapnik&amp;marker=43.57043208931348%2C1.4646202325820923"
+				style="border: 1px solid black"
+			></iframe>
+		</div>
+	</div>
+</section>
+
+<style lang="scss">
+	#find-us {
+		height: 60vh;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		position: relative;
+	}
+
+	img {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+
+	.aero-card {
+		background-color: hsla(202, 63%, 60%, 0.25);
+		border-radius: 4px;
+		padding: 8px;
+		border: 2px solid black;
+		backdrop-filter: blur(5px);
+		box-shadow: 0 0 20px 1px hsl(0, 0, 0, 0.25);
+
+		div {
+			background-color: white;
+			height: 100%;
+			padding: 8px;
+
+			p {
+				margin-top: 0.5rem;
+			}
+
+			iframe {
+				width: 100%;
+				height: 300px;
+			}
+		}
+	}
+</style>
