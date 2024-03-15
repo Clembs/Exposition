f36fe92483bd8a7073e25b185246302d1ed8c3fe
Clément Voisin <clembs@clembs.com>
2024-03-15 22:38:10 +0100

    feat: gallery component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
-	import Billetterie from './Billetterie/Billetterie.svelte';
 	import Hero from './Hero.svelte';
 	import InfosPratiques from './InfosPratiques.svelte';
+	import Gallery from './Gallery.svelte';
+	import Billetterie from './Billetterie/Billetterie.svelte';
 </script>
 
 <main>
 	<Hero />
 	<InfosPratiques />
+	<Gallery />
 	<Billetterie />
 </main>
