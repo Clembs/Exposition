169a793bc4038cc62cd64a93fae5e6a3bece3ed1
Clément Voisin <clembs@clembs.com>
2024-03-16 20:01:58 +0100

    feat: finish up the styling

--- src/routes/attributions/+page.svelte
+++ src/routes/attributions/+page.svelte
@@ -1,19 +1,70 @@
-<ul> 
-    <li><a href="https://pngimg.com/image/68545"> “Typewriter PNG” </a> licensed under <a href="https://pngimg.com/license"> CC BY-NC 4.0</a></li>
-    <li><a href="https://www.publicdomainpictures.net/en/view-image.php?image=296189&picture=telephone-vintage">“Vintage Phone”</a> by Petr Kratochvil licensed under<a href="http://creativecommons.org/publicdomain/zero/1.0/"> CC0 Public Domain </a> </li>
-    <li>Cinématographe 3D model courtesy of <a href="https://mvc-acc.dpt.media/decouvre-les-cameras/cinematographe/"> Ma Caméra et Moi</a></li>
-    <li>IBM 5150 photo courtesy of <a href="https://www.homecomputermuseum.nl/en/collectie/ibm/ibm-5150/"> HomeComputerMuseum </a></li>
-    <li>IBM 5.25" Diskette photo licensed by<a href="https://www.pngegg.com/en/png-wgvca"> PNG EGG </a>  for free non-commercial use</li>
-    <li>Nokia is a registered trademark of Nokia Corporation</li>
-    <li>Facebook and the Facebook logo are trademarks of Meta Platforms Inc.</li>
-    <li>Netflix and the Facebook logo are trademarks of Netflix Inc.</li>
-    <li>Tesla Model 3 image licensed by <a href="https://www.pngplay.com/image/441103"> PNG Play</a> for free non-commercial use</li>
-    <li>ChatGPT and the ChatGPT logo are trademarks of OpenAI LLC.</li>
-    <li>Apple Vision Pro photo courtesy of <a href="https://vr-compare.com/headset/applevisionpro"> VRcompare</a></li>
-    <li>Macintosh, iPhone, iMac, iPod and Apple Vision Pro are trademarks of Apple Inc.</li>
-</ul>
-<style>
-    ul{
-        list-style: inside;
-    }
+<main>
+	<h1>Attributions</h1>
+
+	<ul>
+		<li>
+			<a href="https://pngimg.com/image/68545"> “Typewriter PNG” </a> licensed under
+			<a href="https://pngimg.com/license">CC BY-NC 4.0</a>
+		</li>
+		<li>
+			<a
+				href="https://www.publicdomainpictures.net/en/view-image.php?image=296189&picture=telephone-vintage"
+			>
+				“Vintage Phone”
+			</a>
+			by Petr Kratochvil licensed under
+			<a href="http://creativecommons.org/publicdomain/zero/1.0/">CC0 Public Domain</a>
+		</li>
+		<li>
+			Cinématographe 3D model courtesy of
+			<a href="https://mvc-acc.dpt.media/decouvre-les-cameras/cinematographe/">Ma Caméra et Moi</a>
+		</li>
+		<li>
+			IBM 5150 photo courtesy of
+			<a href="https://www.homecomputermuseum.nl/en/collectie/ibm/ibm-5150/">
+				HomeComputerMuseum
+			</a>
+		</li>
+		<li>
+			IBM 5.25" Diskette photo licensed by <a href="https://www.pngegg.com/en/png-wgvca">
+				PNG EGG
+			</a>
+			for free non-commercial use
+		</li>
+		<li>Nokia is a registered trademark of Nokia Corporation</li>
+		<li>Facebook and the Facebook logo are trademarks of Meta Platforms Inc.</li>
+		<li>Netflix and the Facebook logo are trademarks of Netflix Inc.</li>
+		<li>
+			Tesla Model 3 image licensed by <a href="https://www.pngplay.com/image/441103"> PNG Play</a> for
+			free non-commercial use
+		</li>
+		<li>ChatGPT and the ChatGPT logo are trademarks of OpenAI LLC.</li>
+		<li>
+			Apple Vision Pro photo courtesy of <a href="https://vr-compare.com/headset/applevisionpro">
+				VRcompare
+			</a>
+		</li>
+		<li>Macintosh, iPhone, iMac, iPod and Apple Vision Pro are trademarks of Apple Inc.</li>
+	</ul>
+</main>
+
+<style lang="scss">
+	main {
+		margin: 2rem auto;
+		max-width: 1000px;
+		font-family: var(--font-modern);
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+
+		ul {
+			list-style: inside;
+			border: 2px;
+			width: fit-content;
+		}
+		a {
+			color: var(--color-skype-blue);
+			text-decoration: underline;
+		}
+	}
 </style>
