ecd6738213942bf46a5de030c231110e00432192
Clément Voisin <clembs@clembs.com>
2024-05-24 14:25:13 +0200

    feat: presentation section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
 	import Hero from './Hero.svelte';
+	import Presentation from './Presentation.svelte';
 	import InfosPratiques from './InfosPratiques.svelte';
 	import FindUs from './FindUs.svelte';
 </script>
 
 <main>
 	<Hero />
+	<Presentation />
 	<InfosPratiques />
 	<FindUs />
 </main>
