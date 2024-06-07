520446c47ac255042bca1dc9ae980fcf7cdfd8e0
Clément Voisin <clembs@clembs.com>
2024-06-07 12:20:09 +0200

    feat: page + début mécanographie

---
+++ src/routes/exposition/+page.svelte
@@ -0,0 +1,63 @@
+<script>
+	import Mecanographie from './Mecanographie.svelte';
+</script>
+
+<article>
+	<header>
+		<h1>
+			De la<br /><span class="old-school">mécanique</span> au<br />
+			<span class="retro">numérique</span>
+		</h1>
+
+		<div class="bottom-info">
+			<p>
+				Exposition virtuelle présentée par<br /> Ellisa Ee, Gérald Leban, Kylian Pawilowski et Clément
+				Voisin
+			</p>
+
+			<p>© 2024 Team 20</p>
+		</div>
+	</header>
+
+	<Mecanographie />
+</article>
+
+<style lang="scss">
+	header {
+		color: var(--color-shell-black);
+		font-family: var(--font-modern);
+		font-size: 1.1em;
+		display: flex;
+		flex-direction: column;
+		// align-items: center;
+		justify-content: center;
+		// gap: 1rem;
+		padding: 3rem clamp(1.5rem, 5vw, 3rem);
+		margin: 0 auto;
+		height: 100vh;
+		position: relative;
+
+		h1 {
+			text-align: left;
+			font-size: clamp(3rem, 5vw, 5rem);
+			line-height: initial;
+
+			.old-school {
+				font-size: 1.2em;
+			}
+
+			.retro {
+				font-size: 1.1em;
+			}
+		}
+
+		.bottom-info {
+			position: absolute;
+			bottom: 0;
+			left: 0;
+			padding: clamp(1.5rem, 5vw, 3rem);
+			max-width: 75ch;
+			line-height: 1.5;
+		}
+	}
+</style>
---
+++ src/routes/exposition/Mecanographie.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import ImageModal from '$lib/components/ImageModal.svelte';
+	import tabulatrice from '$lib/images/mecanographie/tabulatrice';
+</script>
+
+<section>
+	<div class="contents">
+		<h2>Partie 1 : la mécanographie</h2>
+		<p>
+			Après la Première Guerre mondiale, l'importation de machines statistiques américaines en
+			Europe marque le début de la mécanographie, principalement utilisée dans les domaines de
+			l'assurance, des statistiques et de l'actuariat. La société qui produisait les machines
+			HOLLERITH, devenue IBM aux États-Unis, était représentée par la Compagnie Electro-Comptable,
+			qui est devenue IBM France en 1947.
+		</p>
+
+		<ImageModal image={tabulatrice} />
+	</div>
+</section>
+
+<style lang="scss">
+	#mécanographie {
+		padding: 5rem 1.5rem;
+
+		color: var(--color-mica-dark);
+		font-family: var(--font-old-school);
+		font-size: 1.25rem;
+		background-color: var(--color-famicom-cream);
+
+		h2 {
+			font-size: 2.5rem;
+		}
+
+		.contents {
+			max-width: 800px;
+			margin: 0 auto;
+		}
+	}
+</style>
