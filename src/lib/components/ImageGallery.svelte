4bd42989ac90234f8aef380dfa5aef5fad0ffe40
Clément Voisin <clembs@clembs.com>
2024-06-09 11:12:26 +0200

    feat: image gallery component

---
+++ src/lib/components/ImageGallery.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import type { Image } from '$lib/images';
+	import ImageModal from './ImageModal.svelte';
+
+	export let images: Image[];
+</script>
+
+<ul class="image-gallery">
+	{#each images as image}
+		<li>
+			<ImageModal {image} />
+		</li>
+	{/each}
+</ul>
+
+<style lang="scss">
+	.image-gallery {
+		--section-width: min(100%, 800px);
+
+		display: flex;
+		overflow-x: scroll;
+		scroll-snap-type: x mandatory;
+		padding: 0;
+		list-style: none;
+
+		align-items: center;
+
+		gap: 1rem;
+
+		width: 100vw;
+		margin: 2rem auto;
+		margin-left: 50%;
+		transform: translateX(-50%);
+
+		scrollbar-width: none;
+		-webkit-overflow-scrolling: touch;
+
+		&::-webkit-scrollbar {
+			display: none;
+		}
+
+		li {
+			scroll-snap-align: center;
+			box-sizing: content-box;
+			width: var(--section-width);
+			flex: 0 0 var(--section-width);
+
+			&:first-child {
+				margin-left: calc(50vw - (var(--section-width) / 2));
+			}
+
+			&:last-child {
+				margin-right: calc(50vw - (var(--section-width) / 2));
+			}
+		}
+	}
+</style>
