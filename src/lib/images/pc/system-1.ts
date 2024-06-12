ab480f99788e410935d2a15d3e37d9a90083166e
Clément Voisin <clembs@clembs.com>
2024-06-13 01:39:00 +0200

    fix: link class on system-1 page

--- src/lib/images/pc/system-1.ts
+++ src/lib/images/pc/system-1.ts
@@ -5,7 +5,7 @@ export default {
 	src: 'https://vignette.wikia.nocookie.net/ipod/images/9/95/System1.jpg/revision/latest?cb=20100819233845',
 	alt: 'Le bureau de Système 1, avec deux fenêtres ouvertes',
 	title: 'Système 1 (Mac OS)',
-	description: `Système 1, sorti en 1984, était le premier système d'exploitation graphique pour ordinateur personnel d'Apple pour accompagner le <a href="https://apple.fandom.com/wiki/Macintosh_128K" target="_blank" rel="noopener">Macintosh</a>. Il se distinguait par son interface utilisateur intuitive, basée sur des métaphores visuelles comme les icônes et les menus déroulants, qui a révolutionné l'interaction homme-machine. Bien que techniquement limité par rapport aux systèmes ultérieurs, Système 1 a posé les bases du succès de Mac OS et a largement influencé le développement des interfaces graphiques modernes.`,
+	description: `Système 1, sorti en 1984, était le premier système d'exploitation graphique pour ordinateur personnel d'Apple pour accompagner le <a class="link" href="https://apple.fandom.com/wiki/Macintosh_128K" target="_blank" rel="noopener">Macintosh</a>. Il se distinguait par son interface utilisateur intuitive, basée sur des métaphores visuelles comme les icônes et les menus déroulants, qui a révolutionné l'interaction homme-machine. Bien que techniquement limité par rapport aux systèmes ultérieurs, Système 1 a posé les bases du succès de Mac OS et a largement influencé le développement des interfaces graphiques modernes.`,
 	sources: [
 		{
 			url: 'https://fr.wikipedia.org/wiki/Syst%C3%A8me_1',
